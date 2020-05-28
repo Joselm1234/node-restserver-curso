be6fe6e4e42a0cb399393d32cf5e274efcbc0164
jose luis Mendoza Rodriguez <joseluismendoza087@gmail.com>
2020-05-27 21:34:07 -0400

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -6,3 +6,24 @@
 //==================================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//=================================
+// Entorno
+//=================================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+//==================================
+//Base de datos
+//==================================
+let urlDB;
+
+//if (process.env.NODE_ENV === 'dev' ) {
+  //  urlDB = 'mongodb://localhost:27017/cafe';
+//}else {
+    urlDB = 'mongodb+srv://jose:Claro123.@cluster0-oov1u.mongodb.net/cafe?retryWrites=true&w=majority';
+
+//}
+process.env.URLDB = urlDB;
+
+
